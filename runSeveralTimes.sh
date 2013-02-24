4075247e25bb54f70ddc1dae5cb580baa553ddd7
Daniel Bittencourt <dcb@dcc.ufam.edu.br>
2013-02-24 16:57:46 +0000

    Making script that runs experiment several times begin from 1
    Signed-off-by: Daniel Bittencourt <dcb@dcc.ufam.edu.br>

--- runSeveralTimes.sh
+++ runSeveralTimes.sh
@@ -2,7 +2,7 @@
 
 runs=$1;
 
-for (( step=0;  step<$runs; step++ ))
+for (( step=1;  step<=$runs; step++ ))
 do
 	echo "At step: " $step " from: "$runs
 	NS_GLOBAL_VALUE="RngRun=$step" ./waf --run "vdt-tap-wifi-virtual-machine --traceFile='/home/daniel/Downloads/vdt/vdt/mobility_traces.txt' --mNodes=17 --sNodes=0 --duration=1800 --logFile=main-ns2-mob.log --sXMLTemplate='/home/daniel/Downloads/vdt/vdt/node.xml' --mXMLTemplate='/home/daniel/Downloads/vdt/vdt/node.xml' --srcImage='/home/daniel/Downloads/vdt/vdt/setup/hydra-node.image'"
