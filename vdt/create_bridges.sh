6755cd8b51865cef5b4e361765a398be679268f6
Daniel Bittencourt <dcb@dcc.ufam.edu.br>
2013-02-25 05:07:44 +0000

    Adding create_bridges helper script
    Signed-off-by: Daniel Bittencourt <dcb@dcc.ufam.edu.br>

---
+++ vdt/create_bridges.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+sudo brctl addbr br000
+sudo brctl addbr br001
+sudo brctl addbr br002
+sudo brctl addbr br003
+sudo brctl addbr br004
+sudo brctl addbr br005
+sudo brctl addbr br006
+sudo brctl addbr br007
+sudo brctl addbr br008
+sudo brctl addbr br009
+sudo brctl addbr br010
+sudo brctl addbr br011
+sudo brctl addbr br012
+sudo brctl addbr br013
+sudo brctl addbr br014
+sudo brctl addbr br015
+sudo brctl addbr br016
+
